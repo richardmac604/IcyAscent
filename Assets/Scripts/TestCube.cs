5ae73082207a7c1dbbcf80c823ef28e4233a7034
Jovan9876 <jovansandhu3@gmail.com>
2024-10-21 16:07:30 -0700

    cube swings

--- Assets/Scripts/TestCube.cs
+++ Assets/Scripts/TestCube.cs
@@ -4,12 +4,29 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 public class TestCube : MonoBehaviour {
-
+    private float rayDistance = 2f;
+    public LayerMask easyClimbLayer;
     [SerializeField] GameObject HingeObject;
     private float speed = 100f;
+    private bool rayHit;
+    private HingeJoint joint;
 
     private void Update() {
-        if (Input.GetMouseButton(0)) {
+         RaycastHit hitPoint = new RaycastHit();
+         rayHit = Input.GetMouseButton(0) && Physics.Raycast(gameObject.transform.position, transform.forward, out hitPoint, rayDistance, easyClimbLayer);
+        if (rayHit) {
+            Debug.Log("RayHit");
+            if (joint == null) {
+            Debug.Log("CreatingJoint");
+                // Create a joint for the left hand and anchor it to the hit point on the wall
+                joint = gameObject.AddComponent<HingeJoint>();
+                joint.axis = Vector3.forward;
+                //joint.connectedBody = rayHit.transform.gameObject.GetComponent<Rigidbody>();  // No connected body, fixed to the wall
+                joint.autoConfigureConnectedAnchor = true;
+                //joint.anchor = hitPoint.point;  // Anchor at the hand's current position
+                joint.connectedAnchor = hitPoint.point;  // Hand is anchored to the hit point on the wall
+            }
+
             Debug.Log("Holding mouse down");
             float horizontalInput = Input.GetAxis("Mouse X") * speed;
             horizontalInput *= Time.deltaTime;
