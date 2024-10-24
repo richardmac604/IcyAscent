8560ed58fe07a88b2e6b7c15e65af9c566bdae09
s-Kone <Connie-jay@hotmail.com>
2024-10-23 20:58:46 -0700

    min start ele changed

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -11,7 +11,7 @@ public class ScoreManager : MonoBehaviour {
     // Start is called before the first frame update
     void Start()
     {
-        highestEle = currentEle.position.y;
+        highestEle = 0;
     }
 
     // Update is called once per frame
